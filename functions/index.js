1cbc3694bb6331bb9e4e7a9366838b8ea977fa16
Jesse Washburn <142361664+jessewashburn@users.noreply.github.com>
2025-11-05 16:28:38 -0500

    switched to myplanet as default jules repo

--- functions/index.js
+++ functions/index.js
@@ -78,7 +78,7 @@ exports.runJules = functions.https.onCall(async (data, context) => {
       title: "Prompt-Sharing Trigger",
       prompt: promptText,
       sourceContext: {
-        source: "sources/github/open-learning-exchange/planet",
+        source: "sources/github/open-learning-exchange/myplanet",
         githubRepoContext: { startingBranch: "master" }
       },
       automationMode: "AUTO_CREATE_PR"
@@ -179,7 +179,7 @@ exports.runJulesHttp = functions.https.onRequest(async (req, res) => {
       title: "Prompt-Sharing Trigger",
       prompt: promptText,
       sourceContext: {
-        source: "sources/github/open-learning-exchange/planet",
+        source: "sources/github/open-learning-exchange/myplanet",
         githubRepoContext: { startingBranch: "master" }
       },
       automationMode: "AUTO_CREATE_PR"
