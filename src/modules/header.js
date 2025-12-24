70115b4d420e8d232438ec2355b4eb190273fbed
Jesse Washburn <142361664+jessewashburn@users.noreply.github.com>
2025-12-24 14:56:27 -0500

    hotfix: relative urls

--- src/modules/header.js
+++ src/modules/header.js
@@ -1,6 +1,6 @@
 export async function loadHeader() {
   try {
-    const response = await fetch('/header.html');
+    const response = await fetch('./header.html');
     const headerHtml = await response.text();
     
     // Insert header at the beginning of body
