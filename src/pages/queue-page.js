f2c09c645fbe92f4b1963fcad508e4d593475708
Jesse Washburn <142361664+jessewashburn@users.noreply.github.com>
2026-01-18 02:23:26 -0500

    smoother queue

--- src/pages/queue-page.js
+++ src/pages/queue-page.js
@@ -5,14 +5,12 @@
 
 import { initMutualExclusivity } from '../utils/checkbox-helpers.js';
 import { attachQueueHandlers, listJulesQueue, renderQueueListDirectly } from '../modules/jules-queue.js';
-import { loadSubtaskErrorModal } from '../modules/jules-modal.js';
 import { TIMEOUTS } from '../utils/constants.js';
 
 // Initialize checkbox mutual exclusivity
 initMutualExclusivity();
 
-// Load the subtask error modal partial
-loadSubtaskErrorModal();
+// Note: Subtask error modal is lazy loaded when needed
 
 function waitForComponents() {
   if (document.querySelector('header')) {
